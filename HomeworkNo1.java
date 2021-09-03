a462ad1280fefe26ee10623ddc96583a34e0be83
Eduard-DavidyanBDG <82771951+Eduard-DavidyanBDG@users.noreply.github.com>
2021-09-03 14:41:59 -0700

    Add files via upload

---
+++ HomeworkNo1.java
@@ -0,0 +1,34 @@
+public class HomeworkNo1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Eduard");
+        int a = 76;
+        int b = 1190;
+        int c;
+        c = a + b;
+        System.out.println(c);
+        a = 834;
+        b = 2791;
+        System.out.println(c);
+        a = 246;
+        b = 5;
+        c = a/b;
+        System.out.println(c);
+        a = 1005;
+        b = 28;
+        c = a/b;
+        System.out.println(c);
+        c = -5 + 8 * 6;
+        System.out.println(c);
+        c = (55+9) % 9;
+        System.out.println(c);
+        c = 20 + -3*5 / 8;
+        System.out.println(c);
+        c =  5 + 15 / 3 * 2 - 8 % 3;
+        System.out.println(c);
+
+
+
+
+    }
+}
